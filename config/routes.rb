e2f556b72a3d7591ee2c5377a4b611d5e55b54f9
Raghav Sood <raghav_sood@hotmail.com>
2018-12-08 05:14:35 -0500

    modified routes to use new definitions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+#  get 'static_pages/help'               # below is almost same as this one except that,
+    get '/help', to: 'static_pages#help' # this way it will create 2 named routes help_path and help_url
+                                        # which allows us to refer to routes by
+                                            # name rathar than by the raw URL.
+                                        # Use help_url for redirects and help_path in all other instances.
+                                        
+    get '/about', to: 'static_pages#about'
+    get '/contact', to: 'static_pages#contact'
 
-  get 'static_pages/about'
+#  get 'static_pages/home'
+#  get 'static_pages/about'
+#  get 'static_pages/contact'
 
 #    root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
